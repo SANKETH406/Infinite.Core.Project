2081b08a2b76ce5b5c182e802d9fbccf29236770
Sanketh Gajibunda <sankethg@infinite.com>
2023-01-24 12:00:56 +0530

    Add project files.

---
+++ Infinite.Core.Project/Controllers/MoviesController1.cs
@@ -0,0 +1,30 @@
+﻿using Infinite.Core.Project.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Threading.Tasks;
+namespace Infinite.Core.Project.Controllers
+{
+    public class MoviesController : Controller
+    {
+        private readonly IConfiguration _configuration; public MoviesController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<MovieViewModel> movies = new();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new System.Uri(_configuration["ApiUrl:api"]);
+                var result = await client.GetAsync("Movies/GetAllMovies");
+                if (result.IsSuccessStatusCode)
+                {
+                    movies = await result.Content.ReadAsAsync<List<MovieViewModel>>();
+                }
+            }
+            return View(movies);
+        }
+    }
+}
---
+++ Infinite.Core.Project/Models/MovieViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Infinite.Core.Project.Models
+{
+    public class MovieViewModel
+    {
+        public int Id { get; set; }
+        public string MovieName { get; set; }
+        public string ProductionName { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public int GenreId { get; set; }
+    }
+}
